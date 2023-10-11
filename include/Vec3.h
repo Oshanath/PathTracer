c9c4120186e2fb1759e8720017a947b4e4bd6c92
Oshanath Rajawasam <oshanath.19@cse.mrt.ac.lk>
2023-10-11 22:42:29 +0530

    dielectrics

--- include/Vec3.h
+++ include/Vec3.h
@@ -92,6 +92,13 @@ public:
         return v - 2 * dot(v, n) * n;
     }
 
+    inline static Vec3 refract(const Vec3& uv, const Vec3& n, double etai_over_etat) {
+        auto cos_theta = fmin(dot(-uv, n), 1.0);
+        Vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+        Vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+        return r_out_perp + r_out_parallel;
+    }
+
     friend std::ostream& operator<<(std::ostream& out, const Vec3& v);
     friend Vec3 operator+(const Vec3& u, const Vec3& v);
     friend Vec3 operator-(const Vec3& u, const Vec3& v);
